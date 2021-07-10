0e5498b81facd4eed90227933a68f037bcd1c7b1
Rick <dirklo@gmail.com>
2021-07-10 21:37:03 +0200

    render out seed data to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController 
     def index
-        @temperature_entries = ajax({
-            url: "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=#{ENV['API_KEY']}&q=30.404251,-97.849442&data=weather",
-            type: "get"
-        })
-        byebug
+        @days = Day.all
         render 'home/index'
     end
 end
