362415de4d0c876aa10c16b0f09f5484caa64bfd
Rick <dirklo@gmail.com>
2021-07-10 21:28:02 +0200

    add comments and delete test JS file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,19 @@ require 'rest-client'
 require 'open-uri'
 require 'date'
 
+
+# Backlog data from 1 month ago, to populate database
 date_today = Date.today.strftime
 one_month_ago = (Date.today - 30).strftime
 
 url = "http://api.worldweatheronline.com/premium/v1/past-weather.ashx?key=#{ENV['API_KEY']}&q=30.404251,-97.849442&data=weather&date=#{one_month_ago}&enddate=#{date_today}&tp=1&format=json"
 weather_data = open(url).read
 parsed_weather_data = JSON.parse(weather_data)
-
 all_weather_data = parsed_weather_data['data']['weather']
 
+# Creating DB objects:
+# Day(date)
+# TemperatureEntry(tempf, tempc, hour)
 all_weather_data.each do |day|
     @day = Day.create(date: day['date'])
     day['hourly'].each do |hour| 
