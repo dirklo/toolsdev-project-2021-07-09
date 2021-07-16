c8edde4510c16259f4eccb4a7ecf1b6321f3256c
Rick <dirklo@gmail.com>
2021-07-16 18:07:21 +0200

    add 48 hour limit to forecast entries

--- app/controllers/temperature_entries_controller.rb
+++ app/controllers/temperature_entries_controller.rb
@@ -1,5 +1,5 @@
 class TemperatureEntriesController < ApplicationController
     def index
-        render json: TemperatureEntry.less_than_1_month_old.sort_by_date
+        render json: TemperatureEntry.less_than_1_month_old.less_than_48_hours_forecast.sort_by_date
     end
 end
--- app/models/temperature_entry.rb
+++ app/models/temperature_entry.rb
@@ -8,8 +8,9 @@ class TemperatureEntry < ApplicationRecord
 
     scope :sort_by_date, -> { order(:date) }
     scope :less_than_1_month_old, -> { where('date >= ?', Time.new - 1.month) }
-    scope :historical, -> { where('date <= ?', Time.new) }
-    scope :forecast, -> { where('date > ? AND date < ?', Time.new, Time.new + 2.days) }
+    # scope :historical, -> { where('date <= ?', Time.new) }
+    # scope :forecast, -> { where('date > ? AND date < ?', Time.new, Time.new + 2.days) }
+    scope :less_than_48_hours_forecast, -> { where('date > ? AND date < ?', Time.new, Time.new + 48.hours) }
     scope :stale, -> { where('date <=? AND historical = ?', Time.new, false) }
 
     # Pass in a url to create all ruby object from the api call
