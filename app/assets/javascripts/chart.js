e5b882b188057f26983b9feaad348df305d439b6
Rick <dirklo@gmail.com>
2021-07-17 14:14:20 +0200

    convert chart.js to use AJAX

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -2,152 +2,151 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Fetch data from database and build chart options objects
     async function buildChartOptions() {
-        const res = await fetch('/temperature_entries')
-        const data = await res.json()
+        const data = await $.ajax({url: '/temperature_entries' })
 
-        // Historical datapoints
-        const historicalEntries = data.filter(data => data.historical)
-        const historicalDataF = historicalEntries.map(entry => entry.tempf)
-        const historicalDataC = historicalEntries.map(entry => entry.tempc)
-        const historicalStartDate = new Date(historicalEntries[0].date)
-        
-        // Forecast datapoints
-        const forecastEntries = data.filter(data => !data.historical)
-        const forecastDataF = forecastEntries.map(entry => entry.tempf)
-        const forecastDataC = forecastEntries.map(entry => entry.tempc)
-        const forecastStartDate = new Date(forecastEntries[0].date)
+            // Historical datapoints
+            const historicalEntries = data.filter(data => data.historical)
+            const historicalDataF = historicalEntries.map(entry => entry.tempf)
+            const historicalDataC = historicalEntries.map(entry => entry.tempc)
+            const historicalStartDate = new Date(historicalEntries[0].date)
+            
+            // Forecast datapoints
+            const forecastEntries = data.filter(data => !data.historical)
+            const forecastDataF = forecastEntries.map(entry => entry.tempf)
+            const forecastDataC = forecastEntries.map(entry => entry.tempc)
+            const forecastStartDate = new Date(forecastEntries[0].date)
 
-        // Records datapoints
-        const threeHourIntervals = historicalEntries.filter(entry => new Date(entry.date).getHours() % 3 === 0)
+            // Records datapoints
+            const threeHourIntervals = historicalEntries.filter(entry => new Date(entry.date).getHours() % 3 === 0)
 
-        const recordsZipped = threeHourIntervals.map(entry => {
-            return {low: entry.record_lowf, high: entry.record_highf}
-        })
-        const recordsStartDate = new Date(threeHourIntervals[0].date)
+            const recordsZipped = threeHourIntervals.map(entry => {
+                return {low: entry.record_lowf, high: entry.record_highf}
+            })
+            const recordsStartDate = new Date(threeHourIntervals[0].date)
 
-        // Compile datapoints into options objects
-        const optionsOne = {
-            chart: {
-                renderTo: 'chart_one',
-                type: 'line'
-            },
-            rangeSelector: {
-                selected: 2,
-                buttons: [{
-                    type: 'day',
-                    count: 1,
-                    text: 'cd',
-                    title: 'View Current Day'
-                }, {
-                    type: 'week',
-                    count: 1,
-                    text: '1w',
-                    title: 'View 1 Week'
-                }, {
-                    type: 'all',
-                    text: 'All',
-                    title: 'View all'
-                }]
-            },
-            title: {
-                text: 'Austin HQ Temperature'
-            },
-            subtitle: {
-                text: 'Previous Month at 1-Hour Intervals'
-            },
-            xAxis: {
-                type: 'datetime'
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature(C/F)'
+            // Compile datapoints into options objects
+            const optionsOne = {
+                chart: {
+                    renderTo: 'chart_one',
+                    type: 'line'
                 },
-            },
-            series: [{
-                name: 'Historical Temperature(F)',
-                data: historicalDataF,
-                pointStart: Date.parse(historicalStartDate),
-                pointInterval: 3600 * 1000 // one hour
-            },
-            {
-                name: 'Forecasted Temperature(F)',
-                data: forecastDataF,
-                pointStart: Date.parse(forecastStartDate),
-                pointInterval: 3600 * 1000  // one hour
-            },
-            {
-                name: 'Historical Temperature(C)',
-                data: historicalDataC,
-                pointStart: Date.parse(historicalStartDate),
-                pointInterval: 3600 * 1000 // one hour
-            },
-            {
-                name: 'Forecasted Temperature(C)',
-                data: forecastDataC,
-                pointStart: Date.parse(forecastStartDate),
-                pointInterval: 3600 * 1000  // one hour
-            }]
-        }
-
-        const optionsTwo = {
-            chart: {
-                renderTo: chart_two,
-                type: 'arearange'
-            },
-
-            rangeSelector: {
-                selected: 2,
-                buttons: [{
-                    type: 'day',
-                    count: 1,
-                    text: 'cd',
-                    title: 'View Current Day'
-                }, {
-                    type: 'week',
-                    count: 1,
-                    text: '1w',
-                    title: 'View 1 Week'
-                }, {
-                    type: 'all',
-                    text: 'All',
-                    title: 'View all'
-                }]
-            },
-        
-            legend: {
-                enabled: false
-            },
-        
-            title: {
-                text: 'Record Highs and Lows'
-            },
-
-            subtitle: {
-                text: '1 Month, 3-hour Intervals'
-            },
-        
-            tooltip: {
-                shared: true
-            },
-        
-            xAxis: {
-                type: 'datetime'
-            },
-        
-            yAxis: {
+                rangeSelector: {
+                    selected: 2,
+                    buttons: [{
+                        type: 'day',
+                        count: 1,
+                        text: 'cd',
+                        title: 'View Current Day'
+                    }, {
+                        type: 'week',
+                        count: 1,
+                        text: '1w',
+                        title: 'View 1 Week'
+                    }, {
+                        type: 'all',
+                        text: 'All',
+                        title: 'View all'
+                    }]
+                },
                 title: {
-                    text: 'Temperature(F)'
-                }
-            },
-        
-            series: [{
-                name: 'Record Highs and Lows',
-                data: recordsZipped,
-                pointStart: Date.parse(recordsStartDate),
-                pointInterval: 3600 * 1000 * 3  // three hours
-            }]
-        }
-        return {optionsOne: optionsOne, optionsTwo: optionsTwo}
+                    text: 'Austin HQ Temperature'
+                },
+                subtitle: {
+                    text: 'Previous Month at 1-Hour Intervals'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Temperature(C/F)'
+                    },
+                },
+                series: [{
+                    name: 'Historical Temperature(F)',
+                    data: historicalDataF,
+                    pointStart: Date.parse(historicalStartDate),
+                    pointInterval: 3600 * 1000 // one hour
+                },
+                {
+                    name: 'Forecasted Temperature(F)',
+                    data: forecastDataF,
+                    pointStart: Date.parse(forecastStartDate),
+                    pointInterval: 3600 * 1000  // one hour
+                },
+                {
+                    name: 'Historical Temperature(C)',
+                    data: historicalDataC,
+                    pointStart: Date.parse(historicalStartDate),
+                    pointInterval: 3600 * 1000 // one hour
+                },
+                {
+                    name: 'Forecasted Temperature(C)',
+                    data: forecastDataC,
+                    pointStart: Date.parse(forecastStartDate),
+                    pointInterval: 3600 * 1000  // one hour
+                }]
+            }
+
+            const optionsTwo = {
+                chart: {
+                    renderTo: chart_two,
+                    type: 'arearange'
+                },
+
+                rangeSelector: {
+                    selected: 2,
+                    buttons: [{
+                        type: 'day',
+                        count: 1,
+                        text: 'cd',
+                        title: 'View Current Day'
+                    }, {
+                        type: 'week',
+                        count: 1,
+                        text: '1w',
+                        title: 'View 1 Week'
+                    }, {
+                        type: 'all',
+                        text: 'All',
+                        title: 'View all'
+                    }]
+                },
+            
+                legend: {
+                    enabled: false
+                },
+            
+                title: {
+                    text: 'Record Highs and Lows'
+                },
+
+                subtitle: {
+                    text: '1 Month, 3-hour Intervals'
+                },
+            
+                tooltip: {
+                    shared: true
+                },
+            
+                xAxis: {
+                    type: 'datetime'
+                },
+            
+                yAxis: {
+                    title: {
+                        text: 'Temperature(F)'
+                    }
+                },
+            
+                series: [{
+                    name: 'Record Highs and Lows',
+                    data: recordsZipped,
+                    pointStart: Date.parse(recordsStartDate),
+                    pointInterval: 3600 * 1000 * 3  // three hours
+                }]
+            }
+            return {optionsOne: optionsOne, optionsTwo: optionsTwo}
     }
 
     // Call to build option objects and apply to charts
